dd3962e5d6da71434849dd5b8588074044aa6fa2
Valdir Botingnon <vbotingnon@neoassist.com>
2021-02-09 10:48:44 -0300

    map and character

---
+++ src/scenes/Game.ts
@@ -0,0 +1,118 @@
+import Phaser from "phaser";
+
+import { debugDraw } from "../utils/debug";
+
+export default class Game extends Phaser.Scene {
+  private cursors!: Phaser.Types.Input.Keyboard.CursorKeys;
+  private faune!: Phaser.Physics.Arcade.Sprite;
+
+  constructor() {
+    super("game");
+  }
+
+  preload() {
+    this.cursors = this.input.keyboard.createCursorKeys();
+  }
+
+  create() {
+    const map = this.make.tilemap({ key: "dungeon" });
+    const tileset = map.addTilesetImage("dungeon", "tiles", 16, 16, 1, 2);
+
+    map.createLayer("Ground", tileset);
+    const wallslayer = map.createLayer("Walls", tileset);
+
+    wallslayer.setCollisionByProperty({ collides: true });
+
+    debugDraw(wallslayer, this);
+
+    this.faune = this.physics.add.sprite(128, 128, "faune", "walk-down-3.png");
+    this.faune.body.setSize(this.faune.width * 0.5, this.faune.height * 0.8);
+
+    this.anims.create({
+      key: "faune-idle-down",
+      frames: [{ key: "faune", frame: "walk-down-3.png" }],
+    });
+
+    this.anims.create({
+      key: "faune-idle-up",
+      frames: [{ key: "faune", frame: "walk-up-3.png" }],
+    });
+
+    this.anims.create({
+      key: "faune-idle-side",
+      frames: [{ key: "faune", frame: "walk-side-3.png" }],
+    });
+
+    this.anims.create({
+      key: "faune-run-down",
+      frames: this.anims.generateFrameNames("faune", {
+        start: 1,
+        end: 8,
+        prefix: "run-down-",
+        suffix: ".png",
+      }),
+      repeat: -1,
+      frameRate: 15,
+    });
+
+    this.anims.create({
+      key: "faune-run-up",
+      frames: this.anims.generateFrameNames("faune", {
+        start: 1,
+        end: 8,
+        prefix: "run-up-",
+        suffix: ".png",
+      }),
+      repeat: -1,
+      frameRate: 15,
+    });
+
+    this.anims.create({
+      key: "faune-run-side",
+      frames: this.anims.generateFrameNames("faune", {
+        start: 1,
+        end: 8,
+        prefix: "run-side-",
+        suffix: ".png",
+      }),
+      repeat: -1,
+      frameRate: 15,
+    });
+
+    this.faune.anims.play("faune-idle-down");
+
+    this.physics.add.collider(this.faune, wallslayer);
+
+    this.cameras.main.startFollow(this.faune, true);
+  }
+
+  update(t: number, dt: number) {
+    if (!this.cursors || !this.faune) {
+      return;
+    }
+
+    const speed = 100;
+    if (this.cursors.left?.isDown) {
+      this.faune.setVelocity(-speed, 0);
+      this.faune.anims.play("faune-run-side", true);
+      this.faune.scaleX = -1;
+      this.faune.body.offset.x = 24;
+    } else if (this.cursors.right?.isDown) {
+      this.faune.setVelocity(speed, 0);
+      this.faune.anims.play("faune-run-side", true);
+      this.faune.scaleX = 1;
+      this.faune.body.offset.x = 8;
+    } else if (this.cursors.up?.isDown) {
+      this.faune.setVelocity(0, -speed);
+      this.faune.anims.play("faune-run-up", true);
+    } else if (this.cursors.down?.isDown) {
+      this.faune.setVelocity(0, speed);
+      this.faune.anims.play("faune-run-down", true);
+    } else {
+      const parts = this.faune.anims.currentAnim.key.split("-");
+      parts[1] = "idle";
+      this.faune.setVelocity(0, 0);
+      this.faune.anims.play(parts.join("-"));
+    }
+  }
+}
---
+++ src/scenes/Preloader.ts
@@ -0,0 +1,19 @@
+import Phaser from "phaser";
+
+export default class Preloader extends Phaser.Scene {
+  constructor() {
+    super("preloader");
+  }
+
+  preload() {
+    // https://github.com/sporadic-labs/tile-extruder
+    this.load.image("tiles", "tiles/dungeon_tiles_extruded.png");
+    this.load.tilemapTiledJSON("dungeon", "tiles/dungeon-01.json");
+
+    this.load.atlas("faune", "character/fauna.png", "character/fauna.json");
+  }
+
+  create() {
+    this.scene.run("game");
+  }
+}
